06eb03490ad0cadc32f93fd9ed191f31c4cda31a
KimJaeYong <kimwpdyd@gmail.com>
2025-07-09 00:09:00 +0900

    불필요한 파일 삭제

--- src/widgets/main/index.ts
+++ src/widgets/main/index.ts
@@ -1,7 +1,5 @@
 export { MainDashboardServer } from './MainDashboardServer';
 export { MainDashboardClient } from './MainDashboardClient';
 export { UserMenu } from './UserMenu';
-export { HomePageClient } from './HomePageClient';
-export { CreateStoryClient } from './CreateStoryClient';
 export { MyStoriesClient } from './MyStoriesClient';
 export { MapRouteSelector } from './MapRouteSelector';
